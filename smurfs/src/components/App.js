1d7c39f32c788e1e0953fe3d893d2101c4823642
garybot <argot.pryer@gmail.com>
2019-11-24 19:56:12 -0800

    First commit, I think I have MVP here.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import "./App.css";
+import { getSmurfs } from '../actions/actions.js';
+import SmurfList from './SmurfList.js';
+import SmurfForm from './SmurfForm.js';
+
 class App extends Component {
   render() {
     return (
@@ -8,9 +13,16 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <button onClick={() => {this.props.getSmurfs()}}>Get Smurfs</button>
+        <SmurfList smurfs={this.props.smurfs} />
+        <SmurfForm />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  smurfs: state.smurfs
+});
+
+export default connect(mapStateToProps, {getSmurfs})(App);
